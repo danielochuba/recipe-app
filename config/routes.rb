928aac5b0d9582f01d837fddb138e169cf67163c
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-28 22:37:19 +0100

    modify routes for users foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+ # get '/users'
+  get 'users/:user_id/foods', to: 'food#index', as: 'user_foods'
+  get 'users/:user_id/foods/new', to: 'food#new', as: 'new_user_food'
+  post 'users/:user_id/foods', to: 'food#create', as: 'create_user_food'
+  delete 'users/:user_id/foods/:id', to: 'food#destroy', as: 'destroy_user_food'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
