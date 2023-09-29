56bb47d710ca074c55cc5021992b21c6c90c88b7
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-29 10:28:53 +0100

    add recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   delete 'users/:user_id/foods/:id', to: 'food#destroy', as: 'destroy_user_food'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'users/:user_id/recipes', to: 'recipe#index', as: 'user_recipes'
+  get 'users/:user_id/recipes/new', to: 'recipe#new', as: 'new_user_recipe'
+  post 'users/:user_id/recipes', to: 'recipe#create', as: 'create_user_recipe'
+  get 'users/:user_id/recipes/:id', to: 'recipe#show', as: 'show_user_recipe'
+  delete 'users/:user_id/recipes/:id', to: 'recipe#destroy', as: 'destroy_user_recipe'
+
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
