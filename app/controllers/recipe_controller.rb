42619d1a7a86a6dc8ff33a8e67deea2bb96b4401
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-29 10:30:28 +0100

    modify recipe controller to handle required actions

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,2 +1,44 @@
 class RecipeController < ApplicationController
+
+  def index
+    @user = User.find(current_user.id)
+    @recipes = @user.recipes
+  end
+
+  def new
+    @recipe = Recipe.new
+
+    respond_to do |format|
+      format.html { render :new, locals: { user: current_user, recipe: @recipe } }
+    end
+  end
+
+  def create
+    @user = User.find(current_user.id)
+    @recipe = @user.recipes.new(recipe_params)
+
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to new_user_recipe_path(@user), notice: 'Recipe was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(current_user.id)
+    @recipe = @user.recipes.find(params[:id])
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_recipes_path(@user), notice: 'Recipe was successfully deleted.' }
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
