cf604f6e36c0ea56171b118b19555e8ef777910d
JuanDBta <simplebetsas@gmail.com>
2023-10-06 18:47:33 -0500

    Added test for Pubic Recipes

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
     factory :recipe do
       name { 'Delicious Recipe' }
       description { 'This is a tasty recipe.' }
+      preparation_time { 30 } 
+      cooking_time { 30 } 
+      public { true }
       user
     end
   end
--- spec/requests/public_recipes_spec.rb
+++ spec/requests/public_recipes_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'PublicRecipes', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/public_recipes/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe PublicRecipesController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  describe 'GET #index' do
+    it 'redirects to the sign-in page' do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
