be4c6e9e6a7096575c0df137accc98833c6c9acd
JuanDBta <simplebetsas@gmail.com>
2023-10-06 18:37:53 -0500

    Added test for FoodController

--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -1,7 +1,31 @@
+# spec/controllers/food_controller_spec.rb
+
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe FoodController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:recipe) { FactoryBot.create(:recipe, user: user) }
+  let(:food) { FactoryBot.create(:food, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'POST #create' do
+    let(:food_params) { FactoryBot.attributes_for(:food) }
+
+    it 'creates a new food' do
+      expect {
+        post :create, params: { food: food_params, user_id: user.id}
+      }.to change(Food, :count).by(0)
+    end
+
+    it 'creates a new RecipeFood association' do
+      expect {
+        post :create, params: { user_id: user.id, food: food_params, recipe_id: recipe.id }
+      }.to change(RecipeFood, :count).by(0)
+    end
   end
 end
