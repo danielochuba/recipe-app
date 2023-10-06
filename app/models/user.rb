8c0a95a80f6fd2926cd0e60993e615bdd4cf642c
JuanDBta <simplebetsas@gmail.com>
2023-10-06 15:00:30 -0500

    Added gems to make tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :food
   has_many :recipes
+
 end
