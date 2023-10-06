71083d98a3e73b0eb85a89cd70a5b69936e7173d
Daniel Ochuba <danielochuba78@gmail.com>
2023-10-06 17:32:02 +0100

    update test structured

--- spec/views/recipe/index.html.erb_spec.rb
+++ spec/views/recipe/index.html.erb_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Please Customize the email and password with yours: public_recipes/index', type: :feature do
-
   describe 'index page' do
     let(:user) { User.create(id: 1, name: 'Daniel Ochuba', email: 'danielochuba78@gmail.com', password: 'Welcome126@') }
 
-    let(:recipe) { Recipe.create(id: 1, preparation_time: 2.5, cooking_time: 2.0, name: 'Sample Recipe', public: true, user_id: user.id) }
-  before do
-    visit user_recipes_path(user)
+    let(:recipe) do
+      Recipe.create(id: 1, preparation_time: 2.5, cooking_time: 2.0, name: 'Sample Recipe', public: true,
+                    user_id: user.id)
+    end
+    before do
+      visit user_recipes_path(user)
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
 
-    # Click the sign-in button (adjust the selector based on your UI)
-    click_button 'Log in'
+      # Click the sign-in button (adjust the selector based on your UI)
+      click_button 'Log in'
+    end
 
+    it 'renders the index view' do
+      expect(page).to have_content('Recipes')
+    end
   end
-
-  it 'renders the index view' do
-    expect(page).to have_content('Recipes')
-  end
-
-end
-
 end
