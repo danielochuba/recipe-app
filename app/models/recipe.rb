cdac170c1bca1e7f32fdbf4c326d9b9d0428f20e
JuanDBta <simplebetsas@gmail.com>
2023-10-06 22:19:37 -0500

    Update Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 end
